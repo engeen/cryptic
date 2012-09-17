88b11d80ed7b22cdf967adb51642db0a8480e6f0
Yuriy Novikov <yury.novicov@gmail.com>
2012-09-18 02:41:38 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ccrm::Application.config.secret_token = '6b661460ae52a3ecb612fa8f65bc8f41d3195ba9e439fa522206fcaa0ea3a487a9ae760691ff4c72d3807cc2ef656bcf1c3505a120e78972e7e97c420c114390'
