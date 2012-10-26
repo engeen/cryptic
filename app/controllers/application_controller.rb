1271c7776d96959a2125321a5b83558913790af9
Yuriy Novikov <yury.novicov@gmail.com>
2012-10-26 19:00:56 +0400

    upd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = 'ru'#params[:locale] || I18n.default_locale
   end
 
   
