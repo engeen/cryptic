bfe4ce8cca354758fe39a87f495294bfe3503096
Yuriy Novikov <yury.novicov@gmail.com>
2012-10-31 02:29:53 +0400

    upd

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ActiveRecord::Base
   has_many :issues
   has_one :profile
   
+  default_scope where("NOT deleted_at IS  ?", nil)
   
   scope :by_role,  lambda {|user_role| includes(:users_accounts).where('users_accounts.role = ?', user_role) unless user_role.nil?}
   scope :by_project_role,  lambda {|user_role| includes(:users_projects).where('users_projects.role = ?', user_role) unless user_role.nil?}
