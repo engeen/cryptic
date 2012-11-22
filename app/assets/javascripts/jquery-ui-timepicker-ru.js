329c6698df58a8276ee62cb9137ddb4499e9612a
Yuriy Novikov <yury.novicov@gmail.com>
2012-11-22 18:48:16 +0400

    upd

---
+++ app/assets/javascripts/jquery-ui-timepicker-ru.js
@@ -0,0 +1,33 @@
+/* Russian translation for the jQuery Timepicker Addon */
+/* Written by Trent Richardson */
+(function($) {
+	$.timepicker.regional['ru'] = {
+		timeOnlyTitle: 'Выберите время',
+		timeText: 'Время',
+		hourText: 'Часы',
+		minuteText: 'Минуты',
+		secondText: 'Секунды',
+		millisecText: 'Миллисекунды',
+		timezoneText: 'Часовой пояс',
+		currentText: 'Сейчас',
+		closeText: 'Закрыть',
+		timeFormat: 'hh:mm tt',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		ampm: false,
+		isRTL: false,
+		monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+			'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+		monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+			'Июл','Авг','Сен','Окт','Ноя','Дек'],
+		dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+		dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],		
+		hourGrid: 1,
+		minuteGrid: 10,
+		hourMin: 10,
+		hourMax: 18,
+		minDate: 0		
+	};
+	$.timepicker.setDefaults($.timepicker.regional['ru']);
+})(jQuery);
